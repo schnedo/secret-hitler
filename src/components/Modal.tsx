16cff0bb74b5b9b7d86d3b23535a54e4edb91d15
schnedo <schnedo@users.noreply.github.com>
2021-04-16 12:42:15 +0200

    use styled components for styling (#18)

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,18 +1,22 @@
 import { FunctionComponent, MouseEventHandler } from "react";
-import { createUseStyles } from "react-jss";
+import styled, { createGlobalStyle } from "styled-components";
 
-const useStyles = createUseStyles({
-  container: {
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    minHeight: "100vh",
-    minWidth: "100vw",
-    backdropFilter: "brightness(40%)",
-    position: "fixed",
-    top: "0",
-  },
-});
+const Global = createGlobalStyle`
+  body {
+    overflow: hidden;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  min-width: 100vw;
+  backdrop-filter: brightness(40%);
+  position: fixed;
+  top: 0;
+`;
 
 export interface ModalProps {
   open: boolean;
@@ -20,7 +24,6 @@ export interface ModalProps {
 }
 
 const Modal: FunctionComponent<ModalProps> = ({ open, onClose, children }) => {
-  const { container } = useStyles();
   const closeContainer: MouseEventHandler = (event) => {
     if (onClose && event.target === event.currentTarget) {
       onClose();
@@ -31,9 +34,10 @@ const Modal: FunctionComponent<ModalProps> = ({ open, onClose, children }) => {
     return <></>;
   }
   return (
-    <div className={container} onClick={closeContainer}>
+    <Container onClick={closeContainer}>
+      <Global />
       <dialog open={open}>{children}</dialog>
-    </div>
+    </Container>
   );
 };
 
