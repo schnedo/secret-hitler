18b7d507bf23cd84f5a9f11e3a40f427ba60aa90
Dominik Schneider <schnedo@users.noreply.github.com>
2021-09-10 11:08:44 +0200

    use dependency injection on ChancellorNomination

--- src/game/election/components/index.ts
+++ src/game/election/components/index.ts
@@ -1,9 +1,10 @@
 import { Modal } from "../../../components";
+import createChancellorNomination from "./createChancellorNomination";
 import createElectionEvaluation from "./createElectionEvaluation";
 
-export { default as ChancellorNomination } from "./ChancellorNomination";
 export { default as FailedElectionCounter } from "./FailedElectionCounter";
 export { default as Voting } from "./Voting";
 export type { VotingProps } from "./Voting";
 
 export const ElectionEvaluation = createElectionEvaluation(Modal);
+export const ChancellorNomination = createChancellorNomination(Modal);
