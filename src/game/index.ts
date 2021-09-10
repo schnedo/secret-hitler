87fb00bba20f2b83fdfb14587c950e37c155c3c7
Dominik Schneider <schnedo@users.noreply.github.com>
2021-09-10 11:47:00 +0200

    use dependency injection on gameReducer

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,24 +1,49 @@
-export type { Government, ElectionRound, VotingProps } from "./election";
-export {
+import { startGame } from "./actions";
+import createGameReducer from "./createGameReducer";
+import {
+  acceptElection,
+  declineElection,
   isValidNomination,
+  nextPresidentialCandidate,
+  nominateChancellor,
+  vote,
+} from "./election";
+import { discardPolicy, playPolicy } from "./legislation";
+import { assignRoles } from "./player";
+
+export * from "./actions";
+export {
+  acceptElection,
   ChancellorNomination,
+  declineElection,
   ElectionEvaluation,
   FailedElectionCounter,
+  isValidNomination,
+  nominateChancellor,
   vote,
   Voting,
 } from "./election";
-export { Avatar } from "./player";
-export type { Player } from "./player";
-export * from "./actions";
-export { default as gameReducer } from "./reducer";
+export type { ElectionRound, Government, VotingProps } from "./election";
 export {
-  policyDeckReducer,
   discardPolicy,
+  DiscardPolicy,
   playPolicy,
   PolicyCardFields,
-  DiscardPolicy,
+  policyDeckReducer,
 } from "./legislation";
 export type { Policy } from "./legislation";
-export { declineElection } from "./election";
-export { acceptElection } from "./election";
-export { nominateChancellor } from "./election";
+export { Avatar } from "./player";
+export type { Player } from "./player";
+
+export const gameReducer = createGameReducer({
+  startGame,
+  nominateChancellor,
+  isValidNomination,
+  nextPresidentialCandidate,
+  vote,
+  acceptElection,
+  declineElection,
+  assignRoles,
+  discardPolicy,
+  playPolicy,
+});
